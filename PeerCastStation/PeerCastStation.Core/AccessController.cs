da42818ea8a823aa90fb1c984fe6ebe8db21723a
Yoteichi <plonk@piano.email.ne.jp>
2016-08-24 12:58:42 +0900

    打ち込みやコピペがしやすいように、生成されるパスワードから記号やまぎらわしい文字を省いた。また、これにより推測されやすくならないように文字数を1つ増やした。

--- PeerCastStation/PeerCastStation.Core/AccessController.cs
+++ PeerCastStation/PeerCastStation.Core/AccessController.cs
@@ -164,10 +164,9 @@ namespace PeerCastStation.Core
   public class AuthenticationKey
   {
     private static char[] KeyCharTable = new char[] {
-      'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-      'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-      '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-      '-', '.', '_', '~', '!', '$', '&', '\'', '(', ')', '*', '+', ',', ';', '=',
+      'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+      'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+      '2', '3', '4', '5', '6', '7', '8', '9',
     };
     private static Random random = new Random();
 
@@ -187,8 +186,8 @@ namespace PeerCastStation.Core
     public static AuthenticationKey Generate()
     {
       return new AuthenticationKey(
-        new String(Enumerable.Range(0, 16).Select(i => KeyCharTable[random.Next(KeyCharTable.Length)]).ToArray()),
-        new String(Enumerable.Range(0, 16).Select(i => KeyCharTable[random.Next(KeyCharTable.Length)]).ToArray())
+        new String(Enumerable.Range(0, 18).Select(i => KeyCharTable[random.Next(KeyCharTable.Length)]).ToArray()),
+        new String(Enumerable.Range(0, 18).Select(i => KeyCharTable[random.Next(KeyCharTable.Length)]).ToArray())
       );
     }
   }
