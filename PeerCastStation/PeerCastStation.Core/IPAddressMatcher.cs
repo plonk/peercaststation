623a264e6f13893f3f201215b9dc0585e57265b9
Yoteichi <plonk@piano.email.ne.jp>
2016-08-29 22:41:09 +0900

    IPAddressMatcherを192.168.0.0/24のような文法にを受け付けるようにして書き直した。

--- PeerCastStation/PeerCastStation.Core/IPAddressMatcher.cs
+++ PeerCastStation/PeerCastStation.Core/IPAddressMatcher.cs
@@ -3,71 +3,67 @@ using System.Linq;
 using System.Text.RegularExpressions;
 using System.Collections.Generic;
 using System.Net;
+using System.Diagnostics;
 
 namespace PeerCastStation {
-
   // IPアドレス/ホスト名のパターンマッチを行なう。MonoだとGetHostEntry等
   // で逆引きが行なわれないので、ホスト名のパターンは使えない。
-  public class IPAddressMatcher
-  {
-    enum PatternType {
-      Hostname,
-      IPv4Address
-    }
-
+  public class IPAddressMatcher {
     public IPAddressMatcher(String expression)
     {
       _stringRepresentation = expression;
       var subexpressions = expression.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
         .Select(exp => exp.Trim());
-      Regex pattern;
-      PatternType type;
 
       foreach (var exp in subexpressions) {
-        var escaped = Regex.Escape(exp);
+        var match = new Regex(@"^(\d+)\.(\d+)\.(\d+)\.(\d+)(?:/(\d+))?$").Match(exp);
 
-        if (new Regex(@"^[\d.]+$").Match(exp).Success) {
-          type = PatternType.IPv4Address;
-        } else if (new Regex(@"^[A-z.\-_]+$").Match(exp).Success) {
-          type = PatternType.Hostname;
+        if (match.Success) {
+          var xs = Enumerable.Range(1, 4).Select(i => uint.Parse(match.Groups[i].Value)).ToArray();
+          foreach (var x in xs) {
+            if (x > 255) {
+              throw new FormatException("octet out of range");
+            }
+          }
+          var ip_address = ToUInt32(xs[0], xs[1], xs[2], xs[3]);
+          int netmask_len;
+          if (match.Groups[5].Value == "") {
+            netmask_len = 32;
+          } else {
+            netmask_len = int.Parse(match.Groups[5].Value);
+          }
+          if (netmask_len > 32) {
+            throw new FormatException("netmask out of range");
+          }
+
+          _networks.Add(Tuple.Create(ip_address, Netmask(netmask_len)));
         } else {
-          throw new FormatException();
+          throw new FormatException("syntax error");
         }
-
-        if (new Regex(@"\.$").Match(exp).Success) {
-          // 前方一致
-          pattern = new Regex($"^{escaped}.+$");
-        }
-        else if (new Regex(@"^\.").Match(exp).Success) {
-          // 後方一致
-          pattern = new Regex($"^.+{escaped}$");
-        }
-        else {
-          // 完全一致
-          pattern = new Regex($"^{escaped}$");
-        }
-
-        _expressions.Add(Tuple.Create(type, pattern));
       }
+    }
+
+    private uint ToUInt32(uint a, uint b, uint c, uint d)
+    {
+      return a << 24 | b << 16 | c << 8 | d;
+    }
+
+    private uint Netmask(int bits) {
+      Debug.Assert(bits >= 0 && bits <= 32);
+      return 0xffffffff >> (32 - bits) << (32 - bits);
     }
 
     public bool Match(IPAddress addr)
     {
-      var addr_str = addr.ToString();
-      foreach (var pair in _expressions) {
-        var type = pair.Item1;
-        var regex = pair.Item2;
+      if (addr.AddressFamily != System.Net.Sockets.AddressFamily.InterNetwork)
+        return false;
 
-        if (type == PatternType.IPv4Address) {
-          if (regex.Match(addr_str).Success) {
-            return true;
-          }
-        } else if (type == PatternType.Hostname) {
-          var host_entry = Dns.GetHostEntry(addr);
-          if (regex.Match(host_entry.HostName).Success) {
-            return true;
-          }
-        }
+      var bytes = addr.GetAddressBytes();
+      var ip_address = ToUInt32(bytes[0], bytes[1], bytes[2], bytes[3]);
+
+      foreach (var network in _networks) {
+        if ((ip_address & network.Item2) ==  network.Item1)
+          return true;
       }
       return false;
     }
@@ -77,8 +73,7 @@ namespace PeerCastStation {
       return _stringRepresentation;
     }
 
-    private List<Tuple<PatternType, Regex>> _expressions = new List<Tuple<PatternType, Regex>>();
+    private List<Tuple<uint, uint>> _networks = new List<Tuple<uint, uint>>();
     private string _stringRepresentation;
   }
 }
-
