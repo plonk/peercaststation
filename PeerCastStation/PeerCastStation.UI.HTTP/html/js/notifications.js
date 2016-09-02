4179e9b98eaa55f93894450ea924cb508f22c81d
Yoteichi <plonk@piano.email.ne.jp>
2016-09-03 02:18:44 +0900

    新しい通知が上に来るようにした。

--- PeerCastStation/PeerCastStation.UI.HTTP/html/js/notifications.js
+++ PeerCastStation/PeerCastStation.UI.HTTP/html/js/notifications.js
@@ -1,36 +1,34 @@
 ﻿var NotificationsViewModel = new function() {
   var self = this;
-  var updating = false;
 
   self.alerts = ko.observableArray([]);
 
   self.update = function() {
     PeerCast.getAllNotificationMessages(function(results) {
-      if (results) {
-        self.alerts.push.apply(self.alerts, $.map(results, function (data) {
-          var alert = "";
-          switch (data.type) {
-          case "info":    alert = "alert-info"; break;
-          case "warning": alert = "alert-danger"; break;
-          case "error":   alert = "alert-error"; break;
-          }
-          var closed = false;
-          return {
-            title:   data.title,
-            message: data.message,
-            type:    data.type,
-            alert:   alert
-          };
-        }));
+      if (!results) {
+        return;
       }
+
+      results.reverse();
+      var new_alerts = $.map(results, function (data) {
+        var closed = false;
+        return {
+          title:   data.title,
+          message: data.message,
+          type:    data.type,
+        };
+      })
+      self.alerts(new_alerts);
     });
   };
 
   self.bind = function(target) {
     self.update();
-    updating = true;
     ko.applyBindings(self, target);
-    updating = false;
   };
+
+  $(function() {
+    setInterval(self.update, 1000);
+  });
 };
 
