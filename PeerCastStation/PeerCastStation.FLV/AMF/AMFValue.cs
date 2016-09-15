c87c5984c7bd7bbb1727597ee2e8e53a8c7d5aa1
Yoteichi <plonk@piano.email.ne.jp>
2016-09-15 13:09:35 +0900

    Revert "AMFValue.Equals の挙動の変更。"
    This reverts commit d37317bc9209dbfdfad0ce6ff4d881fb5a76f8f2.

--- PeerCastStation/PeerCastStation.FLV/AMF/AMFValue.cs
+++ PeerCastStation/PeerCastStation.FLV/AMF/AMFValue.cs
@@ -299,8 +299,7 @@ namespace PeerCastStation.FLV.AMF
 
     public bool Equals(AMFValue obj)
     {
-      if (obj==null) return false;
-      if (obj.Type!=this.Type) return false;
+      if (obj.Type!=obj.Type) return false;
       switch (obj.Type) {
       case AMFValueType.Null:
       case AMFValueType.Undefined:
@@ -313,7 +312,6 @@ namespace PeerCastStation.FLV.AMF
 
     public override bool Equals(object obj)
     {
-      if (obj==null) return false;
       if (obj.GetType()!=this.GetType()) return false;
       return Equals((AMFValue)obj);
     }
